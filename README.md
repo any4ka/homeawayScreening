541a0c70ed794a01fcc72cf156fdf2e82ec25adc
Anna Kruglyanskaya <anna.kruglyanskaya@gmail.com>
2016-02-09 22:51:03 -0600

    updated read me file

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # homeawayScreening
 Part I – Coding Exercise
+
+# Web Application Test
+Assumptions:
+* Used the search feature of the site since "All Products" page did not have the phone required.
+* Multiple implicit waits to make sure that the page had rendered correctly.
+
+Bugs:
+1. When hitting the "Purchase" Button without any information entered, the screen goes back to the shopping cart.
+    Expected Result: Page is to refresh and the errors to appear above the required fields
+2. The State text box disappears after coming back to the page.
+    Expected Result: text box to be present, while the drop down menu is not working.
--- src/test/java/WebAppTest.java
+++ src/test/java/WebAppTest.java
@@ -1,13 +1,13 @@
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeDriverService;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -19,47 +19,88 @@ import java.io.File;
  * Created by Anna Kruglyanskaya on 2/9/2016.
  */
 public class WebAppTest {
-    //Define Objects
-    WebElement allProductLink() {
-        return driver.findElement(By.xpath("//a[contains(.,'All Product')]"));
-    }
-
-    @FindBy(how = How.NAME, using = "s")
-    WebElement searchBox;
-
-    @FindBy(how = How.LINK_TEXT, using = "Apple iPhone 4S 16GB SIM-Free – Black")
-    WebElement product;
-
-    @FindBy(how = How.XPATH, using = "//*[@id=\"single_product_page_container\"]/div[1]/div[2]/form/div[2]/div[1]")
-    WebElement addToCartButton;
-
-    @FindBy(how = How.ID, using = "fancy_notification_content")
-    WebElement confirmationOverlay;
-
-    @FindBy(how = How.XPATH, using = "//*[@id=\"fancy_notification_content\"]/span")
-    WebElement productConfirmation;
-
-    @FindBy(how = How.XPATH, using = "//*[@id=\"fancy_notification_content\"]/a[1]")
-    WebElement goToCheckoutButton;
-
     WebDriver driver;
+    WebDriverWait wait;
 
     static {
         System.setProperty("webdriver.chrome.driver", "C:\\dev\\chromedriver.exe");
     }
 
+    //Define Objects
+    //@FindBy(how = How.XPATH, using = "//a[contains(.,'All Product')]")
+    WebElement allProductLink() {
+        return driver.findElement(By.xpath("//a[contains(.,'All Product')]"));
+    }
+
+    // @FindBy(how = How.NAME, using = "s")
+    WebElement searchBox() {
+        return driver.findElement(By.xpath("//input[@class='search']"));
+    }
+
+    // @FindBy(how = How.LINK_TEXT, using = "Apple iPhone 4S 16GB SIM-Free – Black")
+    WebElement product() {
+        return driver.findElement(By.xpath("//a[contains(.,'Apple iPhone 4S 16GB SIM-Free – Black')]"));
+    }
+
+    WebElement productTitle() {
+        return driver.findElement(By.xpath("//h1[@class='prodtitle']"));
+    }
+
+    //@FindBy(how = How.XPATH, using = "//*[@id=\"single_product_page_container\"]/div[1]/div[2]/form/div[2]/div[1]")
+    WebElement addToCartButton() {
+        return driver.findElement(By.xpath("//input[@value='Add To Cart']"));
+    }
+
+    // @FindBy(how = How.ID, using = "fancy_notification_content")
+    WebElement confirmationOverlay() {
+        return driver.findElement(By.xpath("//div[@id='fancy_notification_content']"));
+    }
+
+    // @FindBy(how = How.XPATH, using = "//*[@id=\"fancy_notification_content\"]/span")
+    WebElement productConfirmation() {
+        return driver.findElement(By.xpath("//span[contains(.,'You just added \"Apple iPhone 4S 16GB SIM-Free - Black\" to your cart.')]"));
+    }
+
+    //@FindBy(how = How.XPATH, using = "//*[@id=\"fancy_notification_content\"]/a[1]")
+    WebElement goToCheckoutButton() {
+        return driver.findElement(By.xpath("//a[@class='go_to_checkout']"));
+    }
+
+    WebElement continueButton() {
+        return driver.findElement(By.xpath("//span[contains(.,'Continue')]"));
+    }
+
+    public void waitForElement(WebElement element) {
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
     @BeforeClass(alwaysRun = true)
     public void setUpDriver() {
         driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 100);
     }
 
     @Test
     public void addProductToCart() throws InterruptedException {
         driver.get("http://store.demoqa.com/");
-        allProductLink().wait();
-        Assert.assertTrue(allProductLink().isDisplayed());
-        searchBox.sendKeys("Apple 4s" + Keys.ENTER);
-        Assert.assertTrue(product.isDisplayed());
+        waitForElement(searchBox());
+        searchBox().sendKeys("Apple 4s" + Keys.ENTER);
+
+        waitForElement(product());
+        Assert.assertTrue(product().getText().contains("Apple iPhone 4S 16GB SIM-Free – Black"));
+        product().click();
+
+        waitForElement(productTitle());
+        Assert.assertTrue(productTitle().getText().contains("Apple iPhone 4S 16GB SIM-Free – Black"));
+
+        addToCartButton().click();
+        waitForElement(confirmationOverlay());
+        Assert.assertTrue(productConfirmation().getText().contains("Apple iPhone 4S 16GB SIM-Free – Black"));
+        goToCheckoutButton().click();
+
+        waitForElement(continueButton());
+        continueButton().click();
+
     }
 
     @AfterClass(alwaysRun = true)
